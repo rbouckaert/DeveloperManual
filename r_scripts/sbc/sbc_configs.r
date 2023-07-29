e8d3838cb324034d5051c13fa5e20e0385eebbf4
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2023-07-29 18:11:43 -0300

    some improvements

--- r_scripts/sbc/sbc_configs.r
+++ r_scripts/sbc/sbc_configs.r
@@ -3,7 +3,7 @@ do_distances <- FALSE # a flag that asks whether tree distances should be comput
 the_seed <- 666 # random seed to ensure replicability of reference_tree
 
 f.burnin <- 0.1 # burn-in fraction
-n.keep <- 400 # how many posterior samples to keep?
+n.keep <- 200 # how many posterior samples to keep?
 
 Ncores <- 8 # number of cores to be used for parallel processing
 
